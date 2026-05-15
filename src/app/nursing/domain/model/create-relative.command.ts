617c8151280eb2b590c29dae4fc4058d19aae83c
Dayro Richard Rios Piñan <dayro.rios.p@gmail.com>
2026-05-15 00:01:37 -0500

    feat(relatives): refactor CreateRelativeCommand to include first name, last name, and resident ID

--- src/app/nursing/domain/model/create-relative.command.ts
+++ src/app/nursing/domain/model/create-relative.command.ts
@@ -1,24 +1,42 @@
 export class CreateRelativeCommand {
-  private _name: string;
+  private _firstName: string;
+  private _lastName: string;
   private _email: string;
+  private _residentId?: number;
 
-  constructor(createRelativeCommand: { name: string, email: string }) {
-    this._name = createRelativeCommand.name;
+  constructor(createRelativeCommand: { firstName: string; lastName: string; email: string; residentId?: number }) {
+    this._firstName = createRelativeCommand.firstName;
+    this._lastName = createRelativeCommand.lastName;
     this._email = createRelativeCommand.email;
+    this._residentId = createRelativeCommand.residentId;
   }
 
+  get firstName(): string {
+    return this._firstName;
+  }
+  set firstName(value: string) {
+    this._firstName = value;
+  }
+  get lastName(): string {
+    return this._lastName;
+  }
+  set lastName(value: string) {
+    this._lastName = value;
+  }
 
-  get name(): string {
-    return this._name;
-  }
-  set name(value: string) {
-    this._name = value;
-  }
   get email(): string {
     return this._email;
   }
   set email(value: string) {
     this._email = value;
+  }
+
+  get residentId(): number | undefined {
+    return this._residentId;
+  }
+
+  set residentId(value: number | undefined) {
+    this._residentId = value;
   }
 
 }
