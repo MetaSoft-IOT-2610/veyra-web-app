5c9f6eaee491b528f07b70ba84188bb7a20ca558
RenatoCY <renato.calvo2@hotmail.com>
2026-06-08 12:01:38 -0500

    feat(tracking): add unassign device dialog and device detail components

---
+++ src/app/tracking/presentation/components/unassign-device-dialog/unassign-device-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unassign-device-dialog',
+  imports: [],
+  templateUrl: './unassign-device-dialog.html',
+  styleUrl: './unassign-device-dialog.css',
+})
+export class UnassignDeviceDialog {
+
+}
---
+++ src/app/tracking/presentation/views/device-detail/device-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device-detail',
+  imports: [],
+  templateUrl: './device-detail.html',
+  styleUrl: './device-detail.css',
+})
+export class DeviceDetail {
+
+}
